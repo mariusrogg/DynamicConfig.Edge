7e708ae05132833c498a8abf9adf674d469564a7
Marius Roggenbuck <marius.rog@gmx.de>
2023-09-16 22:01:08 +0200

    Added base input connector

---
+++ include/BaseInputConnector.hpp
@@ -0,0 +1,65 @@
+//!
+//! @file BaseInputConnector.hpp
+//! @author Marius Roggenbuck (roggenbuckmarius@gmail.com)
+//! @brief Base class for input variables
+//! @version 0.0.0
+//! @date 2023-09-14
+//!
+//! @copyright Copyright (c) 2023
+//!
+#pragma once
+#include "Connector.hpp"
+#include "EventHandling.hpp"
+
+namespace ModelController
+{
+    template <typename T>
+    class BaseInputConnector : public Connector
+    {
+        private:
+            //!
+            //! @brief Actual value (read from input)
+            //!
+            T actualValue = 0;
+
+        protected:
+            //!
+            //! @brief Set the target value for the input
+            //!
+            //! @param value Target value for input
+            //!
+            void SetValue(T value)
+            {
+                if (actualValue != value)
+                {
+                    actualValue = value;
+                    ValueChangedEvent(GetValue());
+                }
+            }
+
+        public:
+            //!
+            //! @brief Event raised, if value changed
+            //!
+            Event<T> ValueChangedEvent;
+            //!
+            //! @brief Construct a new Base Input object
+            //!
+            //! @param name Name of the connector
+            //! @param config Config of the connector
+            //! @param parent Parent of the Connector (normally pass this)
+            //!
+            BaseInputConnector(std::string name, JsonObject config, Connector* parent = nullptr)
+                : Connector(name, config, parent)
+            { }
+            //!
+            //! @brief Get the actual value set to input
+            //!
+            //! @return T Actual value
+            //!
+            T GetValue() const
+            {
+                return actualValue;
+            }
+    };
+} // namespace ModelController
